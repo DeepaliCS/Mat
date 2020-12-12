b382e38ac8ea23777390782534cf3373704ff7b9
Deepali <deepalik141@gmail.com>
2020-12-12 22:20:43 +0000

    add structure

--- matplot.py
+++ matplot.py
@@ -1,56 +1,41 @@
 
+
+
+class BarCharts:   
+  
+    def __init__(self, title, x_label, y_labels):   
+        self.title = title
+        self.x_label = x_label
+        self.y_labels = y_labels  
+      
+    # Sample Method    
+    def bar_subplot(self):
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+
+        np.random.seed(19680801)
+    
+        plt.rcdefaults()
+        fig, ax = plt.subplots()
+
+        # Example data
+        y_pos = np.arange(len(y_labels))
+
+        performance = 3 + 10 * np.random.rand(len(y_labels))
+        error = np.random.rand(len(y_labels))
+
+        ax.barh(y_pos, performance, xerr=error, align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(y_labels)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel(x_label)
+        ax.set_title(title)
+        plt.show() 
+      
 title = 'How fast do you want to go today?'
 x_label = 'Performance'
 y_labels = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-self.bar_subplot(title, x_label, y_labels)
+p = BarCharts(title, x_label, y_labels)   
+p.bar_subplot()   
 
-def bar_subplot(self, title, x_label, y_labels):
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    # Fixing random state for reproducibility
-    np.random.seed(19680801)
-
-    plt.rcdefaults()
-    fig, ax = plt.subplots()
-
-    # Example data
-    y_pos = np.arange(len(y_labels))
-    performance = 3 + 10 * np.random.rand(len(y_labels))
-    error = np.random.rand(len(y_labels))
-
-    ax.barh(y_pos, performance, xerr=error, align='center')
-    ax.set_yticks(y_pos)
-    ax.set_yticklabels(y_labels)
-    ax.invert_yaxis()  # labels read top-to-bottom
-    ax.set_xlabel(x_label)
-    ax.set_title(title)
-    plt.show()
-
-###################
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# # Fixing random state for reproducibility
-# np.random.seed(19680801)
-
-
-# plt.rcdefaults()
-# fig, ax = plt.subplots()
-
-# # Example data
-# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
-# y_pos = np.arange(len(people))
-# performance = 3 + 10 * np.random.rand(len(people))
-# error = np.random.rand(len(people))
-
-# ax.barh(y_pos, performance, xerr=error, align='center')
-# ax.set_yticks(y_pos)
-# ax.set_yticklabels(people)
-# ax.invert_yaxis()  # labels read top-to-bottom
-# ax.set_xlabel('Performance')
-# ax.set_title('How fast do you want to go today?')
-
-# plt.show()
